99a8db34b7c200bfaff2aae603c06aaed84b71f7
YutaUra <yuuta3594@gmail.com>
2024-09-25 06:57:17 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,29 @@ import { resolve } from "node:path";
 import * as core from "@actions/core";
 import { run } from "./run.js";
 
+const getString = (name: string) => {
+  const value = process.env[name];
+  if (!value) throw new Error(`Missing environment variable: ${name}`);
+  return value;
+};
+const getBoolean = (name: string) => {
+  const value = getString(name);
+  if (["true", "1"].includes(value)) return true;
+  if (["false", "0"].includes(value)) return false;
+  return value.length > 0;
+};
+
 const main = async (): Promise<void> => {
   await run({
-    diff: core
-      .getInput("diff", { required: true })
+    diff: getString("diff")
       .split(" ")
       .map((v) => resolve(v.replace(/^'|'$/g, ""))),
-    message: core.getInput("message", { required: true }),
-    dryRun: core.getBooleanInput("dry-run"),
-    workingDirectory: core.getInput("working-directory"),
-    pnpmWorkspaces: core.getBooleanInput("pnpm-workspaces"),
-    setupGitUser: core.getBooleanInput("setup-git-user"),
-    commitMessage: core.getInput("commit-message"),
+    message: getString("message"),
+    dryRun: getBoolean("dry-run"),
+    workingDirectory: getString("working-directory"),
+    pnpmWorkspaces: getBoolean("pnpm-workspaces"),
+    setupGitUser: getBoolean("setup-git-user"),
+    commitMessage: getString("commit-message"),
   });
 };
 
