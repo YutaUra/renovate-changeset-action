f6d042a8d46b7227d16c75ba7fe62398867c6ea5
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 22:07:40 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/YutaUra/renovate-changeset-action/actions/runs/11022647640

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const getBoolean = (name: string) => {
 };
 
 const main = async (): Promise<void> => {
-  console.log(process.env)
+  console.log(process.env);
   await run({
     diff: getString("diff")
       .split(" ")
